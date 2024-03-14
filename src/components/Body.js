8d9ef30ac5a10edff585df357eee785e327cfcf2
Rahul <rahulthosar363@gmail.com>
2024-03-15 00:27:37 +0530

    create components aside and menu add toggle menu functionality

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,13 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Aside from './Aside/Aside'
 import Main from './Main/Main'
+import MenuContext from './utils/MenuContext'
 
 const Body = () => {
+
+    const [isMenuOpen, setIsMenuOpen] = useState(true)
+
+
   return (
-    <div className='bg-[#14131a] flex w-screen h-screen'>
+    <MenuContext.Provider value={{showMenu: isMenuOpen, setIsMenuOpen}}>
+    <div className='bg-[#14131a] flex w-screen h-screen relative'>
         <Aside/>
         <Main/>
     </div>
+    </MenuContext.Provider>
   )
 }
 
--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,10 +1,37 @@
-import React from 'react'
+import React, { useContext, useState } from 'react'
+import MenuContext from '../utils/MenuContext'
+
 
 const Main = () => {
+
+    const {showMenu, setIsMenuOpen} = useContext(MenuContext)
+   
+    const onClickMenu = ()=>{
+        setIsMenuOpen(!showMenu)
+    }
   return (
-    <div className='border w-[80%] h-full'>
+    <div className={'border w-[80%] h-full flex justify-end'}>
         
-        Main
+        <div>
+            <button onClick={()=>onClickMenu()} className="mt-[100px] text-[#3a3942] py-2 lg:hidden">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                class="flex  cursor-pointer hover:text-[#8b8a91] transition-colors"
+              >
+                <line x1="4" x2="20" y1="12" y2="12"></line>
+                <line x1="4" x2="20" y1="6" y2="6"></line>
+                <line x1="4" x2="20" y1="18" y2="18"></line>
+              </svg>
+            </button>
+          </div>
         
     </div>
   )
